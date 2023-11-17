8a93511c97feccde84b49e4ca3b510770ef1d155
hyukjin Yoon <gurwls0117@gmail.com>
2023-11-17 14:47:12 +0900

    기존 벽돌깨기.c의 외벽과 쟁반 코드 삽입

--- test1/test1.c
+++ test1/test1.c
@@ -6,17 +6,410 @@
 /*#define WALL 1
 #define BOSS 2
 #define SPACE 0*/
+#define delay(n)
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
 #define B_X 12 //보스의 기본 X, Y 좌표
 #define B_Y 7
 #define U_X 14//유저의 기본 X, Y 좌표 
 #define U_Y 20
 
 
-int score_i = 0;
-int life = 1;
-int block_array[21][14]; 
+int ax = -1, ay = -1; //공의 방향을 조정
+int ab_x, ab_y; //공의 위치
+int pre_x, pre_y; //공의 전 위치( 잔상을 없애기 위함)
+int bar_left, bar_right, bar_up_down = 19; //쟁반(플레이어)의 위치
+int Time; //게임 속도
+int score_i = 0; //게임의 점수
+int return_gameover = 0; //게임 다시시작, 1이 되면 게임 다시 시작
+int life = 1; //플레이어의 생명
+int block_array[21][14]; //게임판
+int score_plus = 0;
+enum { HIDDEN, SHOW };
 
 
+void setcolor(int color, int bgcolor)
+{
+    color &= 0xf;
+    bgcolor &= 0xf;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (bgcolor << 4) | color);
+}
+void randomize(void)
+{
+    int i;
+    srand((unsigned)time(NULL));
+    for (i = 0; i < (rand() % RAND_MAX); i++)
+        (rand() % RAND_MAX);
+}
+
+int show_all() //게임판의 모든것을 보여줌
+{
+    int i, j;
+    setcolor(10, 0);
+    for (i = 0; i < 21; i++)
+    {
+        for (j = 0; j < 14; j++)
+        {
+            if (block_array[i][j] == 1)
+            {
+                setcolor(7, 0);
+                printf("■");
+            }
+            else if (block_array[i][j] == 2)
+            {
+                setcolor(10, 0);
+                printf("■");
+                setcolor(7, 0);
+            }
+            else if (block_array[i][j] == 3)
+            {
+                setcolor(13, 0);
+                printf("■");
+                setcolor(7, 0);
+            }
+            else if (block_array[i][j] == 4)
+            {
+                setcolor(12, 0);
+                printf("■");
+                setcolor(7, 0);
+            }
+            else if (block_array[i][j] == 5)
+            {
+                setcolor(5, 0);
+                printf("○");
+                setcolor(7, 0);
+            }
+            else if (block_array[i][j] == 6)
+            {
+                setcolor(8, 0);
+                printf("□");
+                setcolor(7, 0);
+            }
+            else {
+                printf("    ");
+            }
+        }
+        printf("\n");
+    }
+    gotoxy(30, 20); printf("Score : %d", score_i);
+    gotoxy(30, 23); printf("Life : %d", life);
+
+    gotoxy(0, 10);
+    return 0;
+}
+
+int moving_ball(int x, int y) //원래 움직이던 방향에 -1을 곱하여 반대방향으로 움직이도록 함
+{
+    int i, j;
+
+    if (block_array[x][y] == 1) { //공이 벽에 부딪칠 경우
+        if (x >= 0 && y == 0) {   //왼쪽 벽에 부딪혔을 때
+            ay *= (-1);
+            y = y + ay + ay;
+        }
+        if (x >= 0 && y == 13) {  //오른쪽 벽에 부딪혔을 때
+            ay *= (-1);
+            y = y + ay + ay;
+        }
+
+        if (block_array[x + 1][y] == 2 && x == 0) //공과 벽 동시에 부딪혔을때
+        {
+            block_array[x + 1][y] = 0;
+            ax *= (-1);
+            ay *= (-1);
+            x = x + ax + ax;
+            y = y + ay + ay;
+            score_i = Score(score_i);
+        }
+        if (block_array[x + 1][y] == 3 && x == 0) //2번 맞는공과 벽 동시에 부딪혔을때
+        {
+            block_array[x + 1][y] = 2;
+            ax *= (-1);
+            ay *= (-1);
+            x = x + ax + ax;
+            y = y + ay + ay;
+            score_i = Score(score_i);
+        }
+        if (block_array[x + 1][y] == 4 && x == 0) //3번 맞는공과 벽 동시에 부딪혔을때
+        {
+            block_array[x + 1][y] = 3;
+            ax *= (-1);
+            ay *= (-1);
+            x = x + ax + ax;
+            y = y + ay + ay;
+            score_i = Score(score_i);
+        }
+        if (x == 0 && y >= 0) {   //위쪽 벽에 부딪혔을 때
+            ax *= (-1);
+            x = x + ax + ax;
+        }
+        if (x == 20 && y >= 0) {  //아래쪽 벽에 부딪혔을 때
+            life--;    //생명을 하나 감소
+            if (life == -1) {  //남은 생명이 없을 경우
+                game_over();
+                return 0;
+            }
+            else {
+                x = 17;
+                y = 5;
+                ax *= (-1);
+                ay *= (-1);
+                for (i = 0; i < 21; i++)
+                {
+                    for (j = 0; j < 14; j++)
+                    {
+                        if (block_array[i][j] == 6)
+                            block_array[i][j] = 0;
+                    }
+                }
+                bar_up_down = 19;
+                stage_level(select_stage);
+                display_bar(bar_left, bar_right);
+                gotoxy(0, 20);
+                system("PAUSE");
+                gotoxy(0, 10);
+            }
+        }
+    }
+
+    if (block_array[x][y] == 2 || block_array[x][y - ay] == 2 || block_array[x][y] == 3 || block_array[x][y - ay] == 3 || block_array[x][y] == 4 || block_array[x][y - ay] == 4)
+    {       //공이 블럭에 부딪쳤을 때
+        if (ay == (-1))   //공이 우에서 좌로 올라갈 경우
+        {
+            if (block_array[x][y + 1] == 2)  //벽돌에 부딪칠 경우
+            {
+                block_array[x][y + 1] = 0;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y + 1] == 3)  //2번 맞아야 깨지는 벽돌에 부딪칠 경우
+            {
+                block_array[x][y + 1] = 2;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y + 1] == 4)  //3번 맞아야 깨지는 벽돌에 부딪칠 경우
+            {
+                block_array[x][y + 1] = 3;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y] == 4) //3번 맞아야 깨지는 벽돌 모서리에 맞을 경우
+            {
+                block_array[x][y] = 3;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+            else if (block_array[x][y] == 3) //2번 맞아야 깨지는 벽돌 모서리에 맞을 경우
+            {
+                block_array[x][y] = 2;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+            else      //공이 벽돌 모서리에 부딪칠 경우
+            {
+                block_array[x][y] = 0;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+        }
+        else     //공이 좌에서 우로 올라갈 경우
+        {
+            if (block_array[x][y - 1] == 2)
+            {
+                block_array[x][y - 1] = 0;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y - 1] == 3)
+            {
+                block_array[x][y - 1] = 2;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y - 1] == 4)
+            {
+                block_array[x][y - 1] = 3;
+                ax *= (-1);
+                x = x + ax + ax;
+            }
+            else if (block_array[x][y] == 4)
+            {
+                block_array[x][y] = 3;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+            else if (block_array[x][y] == 3)
+            {
+                block_array[x][y] = 2;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+            else
+            {
+                block_array[x][y] = 0;
+                ax *= (-1);
+                ay *= (-1);
+                x = x + ax;
+                y = y + ay;
+            }
+        }
+        score_i = Score(score_i);
+    }
+    if (block_array[x - ax][y] == 2 || block_array[x - ax][y] == 3 || block_array[x - ax][y] == 4)  //벽돌의 벽에 공이 부딪칠 경우
+    {
+        if (ax == (-1))  //벽돌이 상승할때
+        {
+            if (block_array[x + 1][y] == 2)
+                block_array[x + 1][y] = 0;
+            if (block_array[x + 1][y] == 3)
+                block_array[x + 1][y] = 2;
+            if (block_array[x + 1][y] == 4)
+                block_array[x + 1][y] = 3;
+        }
+        else
+        {
+            if (block_array[x - 1][y] == 2)
+                block_array[x - 1][y] = 0;
+            if (block_array[x - 1][y] == 3)
+                block_array[x - 1][y] = 2;
+            if (block_array[x - 1][y] == 4)
+                block_array[x - 1][y] = 3;
+        }
+        ay *= (-1);
+        y = y + ay + ay;
+        score_i = Score(score_i);
+    }
+    if (block_array[x][y] == 6 || block_array[x][y - ay] == 6)   //공이 쟁반에 맞았을 때
+    {
+        if (block_array[x][y] == 6 && block_array[x][y - ay] == 0)
+        {        //공이 쟁반의 모서리에 맞았을 경우
+            ax *= (-1);
+            ay *= (-1);
+            x = x + ax + ax;
+            y = y + ay + ay;
+        }
+        else
+        {
+            ax *= (-1);
+            x = x + ax + ax;
+        }
+        score_plus = 0;
+    }
+    block_array[pre_x][pre_y] = 0;  //공의 잔상을 지움
+    block_array[x][y] = 5;    //다음 움직일 위치에 공 표시
+    pre_x = x;
+    pre_y = y;
+
+    y = y + ay;
+    x = x + ax;
+
+    ab_x = x;
+    ab_y = y;
+}
+
+int display_bar(int left, int right)
+{
+    int i;
+    for (i = left; i <= right; i++)
+        block_array[19][i] = 6;
+}
+int moving_bar(int left, int right, int up_down)
+{
+    int i, j;
+    int ch, temp_l, temp_r, temp_up_down;  //전 위치를 기억, 움직인 잔상을 지우기 위해
+    for (i = left; i <= right; i++)    //쟁반의 길이, 위치 표시
+        block_array[up_down][i] = 6;
+    ch = getch();        //키 입력
+    switch (ch) {
+    case LEFT:        //왼쪽으로 이동
+        if (left == 1) {      //쟁반의 왼쪽 끝으로 이동할 경우 더이상 움직이지 못하게 함
+            return 0;
+        }
+        else {
+            temp_l = left;     //이전 위치를 저장
+            temp_r = right;
+            left--;       //배열의 값을 한나씩 감소 시킴
+            right--;
+            block_array[up_down][left] = 6; //이동한 위치에 쟁반 표시
+            block_array[up_down][right] = 6;
+            block_array[up_down][temp_r] = 0;  // 전 위치 잔상을 지움
+            bar_left = left;     //moving_bar()함수를 호출시 이전값을 기억하기 위해 쟁반의 위치를 기억
+            bar_right = right;
+        }
+        break;
+    case RIGHT:
+        if (right == 12) {
+            return 0;
+        }
+        else {
+            temp_l = left;
+            temp_r = right;
+            left++;
+            right++;
+            block_array[up_down][left] = 6;
+            block_array[up_down][right] = 6;
+            block_array[up_down][temp_l] = 0;
+            bar_left = left;
+            bar_right = right;
+        }
+        break;
+    case UP:
+        if (up_down == 10)      //위로 이동할 수 있는 위치는 10칸으로 제한
+        {
+            bar_up_down = 19;     //쟁반이 가장 위로 이동할 경우 다시 맨 아래로 이동
+            for (j = left; j <= right; j++)  //공이 쟁반을 지났쳤을 경우 공보다 빨리 이동이 불가능 하여 맨 아래쪽으로 이동 시킴
+                block_array[10][j] = 0;
+        }
+        else {
+            temp_up_down = up_down;   //쟁반이 위로 움직일 경우 전위치를 기억
+            up_down--;
+            for (j = left; j <= right; j++) {
+                block_array[up_down][j] = 6; //
+                block_array[temp_up_down][j] = 0;
+            }
+            bar_up_down = up_down;
+        }
+        break;
+    case DOWN:
+        if (up_down == 19)
+            bar_up_down = 19;
+        else {
+            temp_up_down = up_down;
+            up_down++;
+            for (j = left; j <= right; j++) {
+                block_array[up_down][j] = 6;
+                block_array[temp_up_down][j] = 0;
+            }
+            bar_up_down = up_down;
+        }
+        break;
+    }
+}
+
+void cursor(int n) // 커서 보이기 & 숨기기
+{
+    HANDLE hConsole;
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    ConsoleCursor.bVisible = n;
+    ConsoleCursor.dwSize = 1;
+    SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
 typedef struct bullet {//총알의 위치를 알려줄 구조체
     int x, y;
 } bullet;
@@ -43,6 +436,12 @@ typedef struct boss_stat {// 보스의 상태를 알려줄 구조체
 struct user_stat User;
 struct boss_stat Boss;
 
+void gotoxy(int x, int y)
+{
+    COORD Pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
 void gotoxy(int a, int b);
 void cursor(int n);
 void boss(int x, int y);
@@ -53,29 +452,11 @@ int main() {
     cursor(0); //커서 지우기(0)으로 값 초기화.
     srand((unsigned)time(0));
     menu();
-    /*printMap();*/
     return 0;
-
-}void cursor(int n) // 커서 보이기 & 숨기기
-{
-    HANDLE hConsole;
-    CONSOLE_CURSOR_INFO ConsoleCursor;
-
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    ConsoleCursor.bVisible = n;
-    ConsoleCursor.dwSize = 1;
-    SetConsoleCursorInfo(hConsole, &ConsoleCursor);
 }
 
 void boss(int x, int y) {// 보스 출력
-    gotoxy(x, y);         printf("■■■■    ■■■");
-    gotoxy(x + 2, y + 1);   printf("■■■■■■");
-    gotoxy(x, y + 2);      printf("■■■■■■■■");
-    gotoxy(x - 2, y + 3);   printf("■■ ■■■ ■■");
-    gotoxy(x - 3, y + 4);   printf("■■■■■■■■■■■");
-    gotoxy(x - 1, y + 5);   printf("■■     ■■");
-    gotoxy(x + 1, y + 6);   printf("■■ ■■■■");
+    gotoxy(x, y);         printf("■■■■");
 }
 
 void averal() {
@@ -92,16 +473,11 @@ void help() {//도움말
     gotoxy(12, 23); system("pause");
 }
 
-void gotoxy(int x, int y)
-{
-    COORD Pos = { x, y };
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-}
 
 void player(int x, int y) { // 플레이어 출력
     gotoxy(x, y); printf("△");
-    gotoxy(x - 2, y + 1); printf("△■△");
-    gotoxy(x - 2, y + 2); printf("○ ○");
+    gotoxy(x - 2, y + 1); printf(" △■△");
+    gotoxy(x - 2, y + 2); printf(" ○ ○");
 }
 
 void bulletmove();
